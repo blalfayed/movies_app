923f077eae2f049110eb752fa9be700dd74f7a33
blalfayed <fayedblal@yahoo.com>
2025-04-03 02:48:23 +0200

    Create MoviesRepository and Failure,add dartz package

--- lib/movies/domain/repository/base_movies_repository.dart
+++ lib/movies/domain/repository/base_movies_repository.dart
@@ -1,7 +1,10 @@
+import 'package:dartz/dartz.dart';
 import 'package:movies_app/movies/domain/entities/movie.dart';
 
+import '../../../core/error/failure.dart';
+
 abstract class BaseMoviesRepository {
-  Future<List<Movie>> getNowPlaying();
-  Future<List<Movie>> getPopularMovies();
-  Future<List<Movie>> getTopRatedMovies();
+  Future<Either<Failure, List<Movie>>> getNowPlayingMovies();
+  Future<Either<Failure, List<Movie>>> getPopularMovies();
+  Future<Either<Failure, List<Movie>>> getTopRatedMovies();
 }
--- lib/movies/domain/usecases/get_popular_movies_usecase.dart
+++ lib/movies/domain/usecases/get_popular_movies_usecase.dart
@@ -1,10 +1,13 @@
+import 'package:dartz/dartz.dart';
 import 'package:movies_app/movies/domain/entities/movie.dart';
 import 'package:movies_app/movies/domain/repository/base_movies_repository.dart';
+
+import '../../../core/error/failure.dart';
 
 class GetPopularMoviesUsecase {
   BaseMoviesRepository baseMoviesRepository;
   GetPopularMoviesUsecase(this.baseMoviesRepository);
-  Future<List<Movie>> execute() async {
+  Future<Either<Failure, List<Movie>>> execute() async {
     return await baseMoviesRepository.getPopularMovies();
   }
 }
